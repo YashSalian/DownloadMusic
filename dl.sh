ae6e892477e832bf8b487e04fd56a2286b24dbf1
YashSalian <60565151+YashSalian@users.noreply.github.com>
2025-01-06 13:27:32 +0530

    Added QOL options
    Added Help Option for info
    Added Directory Option to create sub-directories for albums.

--- dl.sh
+++ dl.sh
@@ -1,35 +1,58 @@
 #! /bin/bash
 
+help() {
+printf "Usage: ./dl.sh [options] ChannelID \n\n"
+printf "Options:\n"
+printf "  -d, --directory      Create Directories for All Albums"
+exit 0
+}
+
+f=0
+while getopts ":hd:" flag; do
+	case $1 in
+		-d | --directory) f=1 && shift 1;;
+		-h | --help) help | less && exit 0;;
+		\?) echo Invalid Option - "$1" && exit 0;;
+	esac
+done
+
 # The first param will be the ID of the YouTube Channel (For eg, if the ID is @CigarettesAfterSex, then input CigarettesAfterSex, case insensitive)
 
-releases="$(curl -s https://www.youtube.com/@$1/releases | sed s/}/\\n/g |  sed s/playlistRenderer/playlistRenderer\\n/ | grep playlistId | grep simpleText)"
+releases="$(curl -s https://www.youtube.com/@$1/releases | sed s/}/\\n/g |  sed s/playlistRenderer/playlistRenderer\\n/)"
+
 
 # Saving playlistID, Album Name and total count of Albums
-id=$(echo "$releases" | cut -d \" -f5)
-name=$(echo "$releases" | cut -d \" -f11)
+id=$(echo "$releases" | grep playlistId | grep simpleText | cut -d \" -f5)
+name=$(echo "$releases" | grep playlistId | grep simpleText| cut -d \" -f11)
+artist_name=$(echo  "$releases" | grep urlCanonical | cut -d \" -f12 | sed 's/ /\ /g')
 count=$(echo "$releases" | wc -l)
 
 # URL template for input into yt-dlp
 template="https://www.youtube.com/playlist?list="
 
 # Creating Artist's directory
-if [ ! -d "$1" ]; then # If directory does not exists then create it
-	mkdir "$1"
+if [ ! -d "$artist_name" ]; then # If directory does not exists then create it
+	mkdir "$artist_name"
 fi
 
-cd "$1"
+cd "$artist_name"
 
 for (( i = 1 ; i <= $count ; i++ )); do
 	dir=$(echo "$name" | sed -n "$i"p)
 	curr_id=$(echo "$id" | sed -n "$i"p) # | sed s/&/\'&\'/g) # Added in case playlistID contains &, needs to be replaced by \'&\' for yt-dlp without forward slash
-	if [ ! -d "$dir" ]; then # If directory does not exist then create it
-		mkdir "$dir"
+	if [ "$f" = 1 ]; then
+		if [ ! -d "$dir" ]; then # If directory does not exist then create it
+			mkdir "$dir"
+		fi
+		cd "$dir"
 	fi
-	cd "$dir"
-	yt-dlp -x --progress --no-warnings -q --audio-quality 0 --embed-thumbnail "$template$curr_id"
+	echo "$template$curr_id"
+	yt-dlp -x --progress --no-warnings -q --audio-quality m4a --parse-metadata "$dir:%(album)s" --parse-metadata "title:%(title)s - %(artist)s" --embed-metadata --embed-thumbnail "$template$curr_id"
 	for file in *.m4a; do
 		renamed_file=$(echo "$file" | sed 's/ \[[^]]*\]//g') # | sed 's/ /\\ /g')
 		mv "$file" "$renamed_file"
 	done
-	cd ..
+	if [ "$f" = 1 ]; then
+		cd ..
+	fi
 done
